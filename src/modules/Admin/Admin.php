ec28e618f348e1a68931be12a9007ff9e0ee59c8
Daniel Menard 35 <daniel.menard.35@gmail.com>
2012-01-05 13:57:05 +0000

    Module Admin : conversion en utf-8.
    git-svn-id: http://docalist.googlecode.com/svn/Fab2/trunk@94 444a106b-eed5-e7b0-2fd9-c3bf2e9ceeb0

--- src/modules/Admin/Admin.php
+++ src/modules/Admin/Admin.php
@@ -2,7 +2,7 @@
 /**
  * @package     fab
  * @subpackage  Admin
- * @author      Daniel Ménard <Daniel.Menard@bdsp.tm.fr>
+ * @author      Daniel MÃ©nard <Daniel.Menard@bdsp.tm.fr>
  * @version     SVN: $Id: Admin.php 1144 2010-04-08 12:43:31Z daniel.menard.bdsp $
  */
 
@@ -12,13 +12,13 @@
  * Le module Admin constitue le
  * {@link http://fr.wikipedia.org/wiki/Back_office_(informatique) 'BackOffice'}
  * de l'application : il ne comporte {@link actionIndex() qu'une seule action}
- * qui correspond à la page d'accueil du site d'administration de l'application
+ * qui correspond Ã  la page d'accueil du site d'administration de l'application
  * et qui affiche la liste des modules d'administration disponibles.
  *
- * La classe Admin est également la classe ancêtre de tous les autres modules
+ * La classe Admin est Ã©galement la classe ancÃªtre de tous les autres modules
  * d'administration disponibles.
  *
- * Cliquez ici pour accéder au {@link /Admin site d'administration de l'application}.
+ * Cliquez ici pour accÃ©der au {@link /Admin site d'administration de l'application}.
  *
  * @package     fab
  * @subpackage  Admin
@@ -28,19 +28,19 @@ class Admin extends Module
     /**
      * Retourne le titre du module d'administration.
      *
-     * Cette méthode est appellée automatiquement par la
+     * Cette mÃ©thode est appellÃ©e automatiquement par la
      * {@link Admin::actionIndex() page d'accueil} du
      * {@link /Admin site d'administration} pour afficher la liste des modules
      * d'administration disponibles.
      *
-     * Elle retourne le titre indiqué dans la clé
+     * Elle retourne le titre indiquÃ© dans la clÃ©
      * <code><title></code> de la configuration.
      *
-     * Si aucun titre n'est indiqué, elle retourne le nom de la classe en cours.
+     * Si aucun titre n'est indiquÃ©, elle retourne le nom de la classe en cours.
      *
-     * Cette méthode peut être surchargée par les modules d'administration
-     * descendants de cette classe si une implémentation différente est
-     * souhaitée.
+     * Cette mÃ©thode peut Ãªtre surchargÃ©e par les modules d'administration
+     * descendants de cette classe si une implÃ©mentation diffÃ©rente est
+     * souhaitÃ©e.
      *
      * @return string
      */
@@ -55,20 +55,20 @@ class Admin extends Module
     /**
      * Retourne la description du module d'administration.
      *
-     * Cette méthode est appellée automatiquement par la
+     * Cette mÃ©thode est appellÃ©e automatiquement par la
      * {@link Admin::actionIndex() page d'accueil} du
      * {@link /Admin site d'administration} pour afficher la liste des modules
      * d'administration disponibles.
      *
-     * Le but est d'indiquer à l'utilisateur, en quelques lignes, le rôle
+     * Le but est d'indiquer Ã  l'utilisateur, en quelques lignes, le rÃ´le
      * de chacun des modules d'administration.
      *
-     * Par défaut, <code>getDescription()</code> retourne le contenu de la clé
+     * Par dÃ©faut, <code>getDescription()</code> retourne le contenu de la clÃ©
      * <code><description></code> de la configuration ou null si aucune
      * description n'est disponible.
      *
-     * Cette méthode peut être surchargée par les modules d'administration
-     * descendants de cette classe pour générer une description spécifique.
+     * Cette mÃ©thode peut Ãªtre surchargÃ©e par les modules d'administration
+     * descendants de cette classe pour gÃ©nÃ©rer une description spÃ©cifique.
      *
      * @return null|string
      */
@@ -79,20 +79,20 @@ class Admin extends Module
 
 
     /**
-     * Retourne l'url du logo à afficher pour ce module d'administration.
+     * Retourne l'url du logo Ã  afficher pour ce module d'administration.
      *
-     * Cette méthode est appellée automatiquement par la
+     * Cette mÃ©thode est appellÃ©e automatiquement par la
      * {@link Admin::actionIndex() page d'accueil} du
      * {@link /Admin site d'administration} pour afficher la liste des modules
      * d'administration disponibles.
      *
-     * La méthode récupère le nom de l'image indiqué dans la clé
+     * La mÃ©thode rÃ©cupÃ¨re le nom de l'image indiquÃ© dans la clÃ©
      * <code><icon></code> du fichier de configuration.
      *
-     * S'il s'agit d'un chemin relatif, celui-ci est transformé en chemin de la
+     * S'il s'agit d'un chemin relatif, celui-ci est transformÃ© en chemin de la
      * forme <code>/FabWeb/modules/<nom du module>/images/<nom du logo></code>.
      *
-     * Si aucun logo n'est indiqué dans la configuration, la méthode
+     * Si aucun logo n'est indiquÃ© dans la configuration, la mÃ©thode
      * retourne null.
      *
      * @return null|string
@@ -100,7 +100,7 @@ class Admin extends Module
     public function getIcon()
     {
         $icon=Config::get('icon');
-        if ($icon && Utils::isRelativePath($icon)) // fixme: ne pas faire ça ici, intégrer dans le routing
+        if ($icon && Utils::isRelativePath($icon)) // fixme: ne pas faire Ã§a ici, intÃ©grer dans le routing
             $icon='/FabWeb/modules/' . __CLASS__ . '/images/' . $icon;
 
         // fixme: ne marche pas si le module d'administration fait partie de l'application
@@ -112,9 +112,9 @@ class Admin extends Module
      * Retourne un tableau permettant de construire un fil d'ariane
      * (breadcrumbs).
      *
-     * Les clés du tableau retourné contiennent les liens (non routés) des
-     * différentes éléments composant le fil d'ariane. Les valeurs associées
-     * contiennent le libellé à afficher à l'utilisateur.
+     * Les clÃ©s du tableau retournÃ© contiennent les liens (non routÃ©s) des
+     * diffÃ©rentes Ã©lÃ©ments composant le fil d'ariane. Les valeurs associÃ©es
+     * contiennent le libellÃ© Ã  afficher Ã  l'utilisateur.
      *
      * @return array
      * @see getBreadCrumbs()
@@ -138,10 +138,10 @@ class Admin extends Module
      * Retourne un tableau permettant de construire un fil d'ariane
      * (breadcrumbs).
      *
-     * Exemple : Administration » Gestion des fichiers » Modules
+     * Exemple : Administration Â» Gestion des fichiers Â» Modules
      *
-     * La méthode retourne le même tableau que {@link getBreadCrumbsArray()}
-     * si ce n'est que le dernier élément n'a pas de lien (la clé correspondante
+     * La mÃ©thode retourne le mÃªme tableau que {@link getBreadCrumbsArray()}
+     * si ce n'est que le dernier Ã©lÃ©ment n'a pas de lien (la clÃ© correspondante
      * est 0).
      *
      * @return array
@@ -156,55 +156,55 @@ class Admin extends Module
     /**
      * Affiche la liste des modules d'administration disponibles.
      *
-     * Cette action charge chacun des modules indiqués dans la clé
+     * Cette action charge chacun des modules indiquÃ©s dans la clÃ©
      * <code><modules></code> du fichier de configuration et construit un tableau
-     * qui pour chacun des modules trouvés indique :
+     * qui pour chacun des modules trouvÃ©s indique :
      *
      * - <code>title</code> : le titre du module d'administration tel que
-     *   retourné par la méthode {@link getTitle()} de ce module ;
-     * - <code>description</code> : la description telle que retourné par la
-     *   méthode {@link getDescription()} de ce module;
-     * - <code>icon</code> : l'url de l'icone à afficher pour ce module, telle
-     *   que retournée par la méthode {@link getIcon()} de ce module ;
+     *   retournÃ© par la mÃ©thode {@link getTitle()} de ce module ;
+     * - <code>description</code> : la description telle que retournÃ© par la
+     *   mÃ©thode {@link getDescription()} de ce module;
+     * - <code>icon</code> : l'url de l'icone Ã  afficher pour ce module, telle
+     *   que retournÃ©e par la mÃ©thode {@link getIcon()} de ce module ;
      * - <code>link</code> : l'url de l'action index de ce module.
      *
-     * Elle appelle ensuite le template indiqué dans la clé
+     * Elle appelle ensuite le template indiquÃ© dans la clÃ©
      * <code><template></code> du fichier de configuration en lui passant en
-     * paramètre une variable <code>modules</code> contenant le tableau obtenu.
+     * paramÃ¨tre une variable <code>modules</code> contenant le tableau obtenu.
      *
-     * @throws LogicException Une exception est générée si la configuration
+     * @throws LogicException Une exception est gÃ©nÃ©rÃ©e si la configuration
      * indique des modules qui ne sont pas des modules d'administration,
-     * c'est-à-dire des modules qui ne descendent pas de la class
+     * c'est-Ã -dire des modules qui ne descendent pas de la class
      * <code>Admin</code>.
      */
     public function actionIndex()
     {
-        // Détermine le path du template qui sera exécuté
-        // fixme: on est obligé de le faire ici, car on charge un peu plus
-        // bas d'autres modules qui vont écraser notre config et notre searchPath
-        // et du coup, notre template ne sera plus trouvé lors de l'appel
-        // à Template::run
+        // DÃ©termine le path du template qui sera exÃ©cutÃ©
+        // fixme: on est obligÃ© de le faire ici, car on charge un peu plus
+        // bas d'autres modules qui vont Ã©craser notre config et notre searchPath
+        // et du coup, notre template ne sera plus trouvÃ© lors de l'appel
+        // Ã  Template::run
         $template=Utils::searchFile(Config::get('template'));
 
         // sauvegarde notre config
         $config=Config::getAll();
 
-        // Crée la requête utilisée pour charger chacun des modules d'admin
+        // CrÃ©e la requÃªte utilisÃ©e pour charger chacun des modules d'admin
         $request=Request::create()->setAction('');
 
-        // Crée un tableau indiquant pour chacun des modules indiqués dans la
+        // CrÃ©e un tableau indiquant pour chacun des modules indiquÃ©s dans la
         // config : son titre, sa description, l'url de son icone et l'url
         // vers son action index
         $modules=array();
 
         foreach (Config::get('modules') as $moduleName=>$options)
         {
-            // Charge le module indiqué
+            // Charge le module indiquÃ©
             $module=Module::getModuleFor($request->setModule($moduleName));
 
-            // Vérifie que c'est bien un module d'administration
+            // VÃ©rifie que c'est bien un module d'administration
             if (! $module instanceOf Admin)
-                throw new LogicException("Le module $moduleName indiqué dans la config n'est pas un module d'administration.");
+                throw new LogicException("Le module $moduleName indiquÃ© dans la config n'est pas un module d'administration.");
 
             // Ajoute le module dans le tableau
             $modules[$moduleName]=array
@@ -220,7 +220,7 @@ class Admin extends Module
         Config::clear();
         Config::addArray($config);
 
-        // Exécute le template
+        // ExÃ©cute le template
         return Response::create('Html')->setTemplate
         (
             $this,
